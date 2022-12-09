bb9b67ecf48ba41c072db990abe31a9a074cc10a
Lushi Liu <lushiliu1@gmail.com>
2022-12-08 20:26:08 -0700

    Add Files

---
+++ server/index.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const app = express();
+const http = require("http");
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server, {
+    cors: {
+        origin: "http://localhost:3000",
+        methods: ["GET", "POST"],
+    },
+});
+const cors = require("cors");
+
+app.use(cors());
+app.use(express.json());
+
+
+server.listen("3001", ()=> {
+    console.log("Running on 3001");
+});
+
+io.on("connection", (socket) => {
+    console.log(socket.id);
+
+    socket.on("join_room", (data) => {
+        socket.join(data);
+        console.log(`ID ${socket.id} joined room ${data}`);
+    });
+
+    socket.on("sent_message", (data) => {
+        socket.to(data.room).emit("receive_message", data);
+    })
+
+    socket.on("disconnect", () => {
+        console.log("USER DISCONNECTED", socket.id);
+    });
+});
